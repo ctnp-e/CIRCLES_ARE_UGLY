cc279b284370d5c934479e2f8fdbb98f785b5214
masha <283ctnpexe@users.noreply.github.com>
2021-03-06 12:33:26 -0800

    ill do comments later, lick my nuts

--- dontworry.py
+++ dontworry.py
@@ -1,52 +1,112 @@
 import cv2
 import numpy as np
+import random as rng
+import imutils
+import array as arr 
+from pprint import pprint as pp
+
+#need:
+#if contour is smaller than a certain value, ie, in the background, then it isnt counted
+#
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 30)
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-#cap.set(cv2.CAP_PROP_EXPOSURE, -3)
+cap.set(cv2.CAP_PROP_FPS, 15)
+#cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+#cap.set(cv2.CAP_PROP_EXPOSURE, 50)
 x1 = 3
 x2 = 8
+white = (255,255,255)
+lower_orange = np.array([0, 180, 200], np.uint8)
+upper_orange = np.array([30, 255, 255], np.uint8)
+
 kernel = np.ones((x1,x1), np.uint8)
 kernel2 = np.ones((x2,x2), np.uint8)
 
+kernel = np.ones((1,8), np.uint8)
+
+
 while True:
     frame = cap.read()[1]
-    blurred = cv2.medianBlur(frame, 25)
+
     x_size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     y_size = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     black = np.zeros((y_size, x_size, 3), np.uint8)
 
-    mask1 = cv2.inRange(cv2.cvtColor(
-        blurred, cv2.COLOR_BGR2HSV), (100, 150, 0), (140, 255, 255))
-    
-    mask1 = cv2.erode(mask1, kernel)
-    mask2 = cv2.dilate(mask1,kernel2)
 
-    closed = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, kernel2)
+    image = cv2.inRange(cv2.cvtColor(
+        frame, cv2.COLOR_BGR2HSV), lower_orange, upper_orange)
 
-    image = cap.read()[1]
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.medianBlur(gray, 11)
+    image = cv2.erode(image, kernel)
+    image = cv2.dilate(image,kernel2)
 
-    # Morph open 
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    frame = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
-    # Find contours and filter using contour area and aspect ratio
-    cnts = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    ret, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
+
+    # create hull array for convex hull points
+    hull = []
+
+    # calculate points for each contour
+    for i in range(len(contours)):
+        # creating convex hull object for each contour
+        hull.append(cv2.convexHull(contours[i], False))
+
+
+    # create an empty black image
+    drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+
+    # draw contours and hull points
+    for i in range(len(contours)):
+        color_contours = (0, 255, 0) # green - color for contours
+        color = (255, 0, 0) # blue - color for convex hull
+        # draw ith contour
+        cv2.drawContours(frame, contours, i, color_contours, 1, 8, hierarchy)
+        # draw ith convex hull object
+        cv2.drawContours(drawing, hull, i, color, 1, 8)
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    cXarr = []
+    cYarr = []
+
     for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-        area = cv2.contourArea(c)
-        if len(approx) > 5 and area > 1000 and area < 500000:
-            ((x, y), r) = cv2.minEnclosingCircle(c)
-            cv2.circle(image, (int(x), int(y)), int(r), (36, 255, 12), 2)
+        # compute the center of the contour
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cXarr.append(cX)
+        cYarr.append(cY)
+        # draw the contour and center of the shape on the image
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.circle(drawing, (cX, cY), 7, (255, 0, 255), -1)
 
-    cv2.imshow('image', image)
+        cv2.putText(drawing, str(cX), (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        cv2.putText(drawing, str(cY), (cX - 20, cY + 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 2)
+
+    if len(cXarr)==2: #we have two contours
+        #draw the line
+        delta_x = abs(cXarr[0]-cXarr[1])
+        delta_y = abs(cYarr[0]-cXarr[1])
+        cv2.line(drawing,(cXarr[0],cYarr[0]),(cXarr[1],cYarr[1]),(255,0,0),1)
+    else:
+        delta_x = 0
+        delta_y = 0
+
+    cv2.putText(drawing, str(delta_x)+ " - " + str(delta_y), (10,15), 
+    cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 2)
+
 
     cv2.imshow("raw_image", frame)
-    #cv2.imshow("mask1", mask1)
-    cv2.imshow("mask2", mask2)
+    cv2.imshow("mask1", image)
+    cv2.imshow("drawing", drawing)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
