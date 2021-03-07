1fadd04902bd85a4a0d2fb533982a8a19dcdec88
ctnp-e <catnipiscatweed@gmail.com>
2021-03-06 16:23:32 -0800

    colors. ive been doing this for millions of years. fuck you. suck my nuts

--- dontworry.py
+++ dontworry.py
@@ -11,8 +11,8 @@ from pprint import pprint as pp
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS, 15)
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-cap.set(cv2.CAP_PROP_EXPOSURE, 50)
+#cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+#cap.set(cv2.CAP_PROP_EXPOSURE, 50)
 x1 = 3
 x2 = 8
 white = (255,255,255)
@@ -21,7 +21,7 @@ white = (255,255,255)
 # lower_color = np.array([0, 180, 200], np.uint8)
 # upper_color = np.array([30, 255, 255], np.uint8)
 
-#test with the retroreflective
+#test with the retroreflective, assumed blue
 lower_color = np.array([148, 200, 200], np.uint8)
 upper_color = np.array([180, 255, 255], np.uint8)
 
@@ -62,7 +62,7 @@ while True:
 
     # create an empty black image
     drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
-
+    
     # draw contours and hull points
     for i in range(len(contours)):
         color_contours = (0, 255, 0) # green - color for contours
